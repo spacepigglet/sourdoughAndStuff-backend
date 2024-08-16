77f1e54b4c0791719793f6e2f9e2827d1c456c46
Amanda Sjöberg <97839498+spacepigglet@users.noreply.github.com>
2024-08-16 17:29:37 +0200

    Update index.js
    no reload

--- index.js
+++ index.js
@@ -29,20 +29,4 @@ MongoClient.connect(
     })
   })
 
-//reload
 
-const url = 'https://sourdoughandstuff-backend.onrender.com/api/v1/recipes/'; 
-const interval = 60000; // Interval in milliseconds (30 seconds)
-
-function reloadWebsite() {
-  axios.get(url)
-    .then(response => {
-      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
-    })
-    .catch(error => {
-      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
-    });
-}
-
-
-setInterval(reloadWebsite, interval);
